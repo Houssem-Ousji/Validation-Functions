59a78c1888656083be912fecf6fdf2f003ec2a37
HOUSSEM <ousjihoussem01@gmail.com>
2021-07-31 19:05:47 +0200

    test your text

--- is_in_AlphabeticOrder_orNot.js
+++ is_in_AlphabeticOrder_orNot.js
@@ -1,5 +1,5 @@
 function is_in_AlphabeticOrder_orNot(text) {
     opposite_text = text.split('').sort().join('');
     console.log(opposite_text);
-    if (opposite_text == text) { return true } else { return false }
+    if (opposite_text == text) { return true; } else { return false; }
 };
