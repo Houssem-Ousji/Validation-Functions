6562a365fe5a6cbb706f34375acf000a6ff64bbc
HOUSSEM <ousjihoussem01@gmail.com>
2021-08-11 00:49:37 +0200

    test your adress

---
+++ is_ip_adress_OrNot.js
@@ -0,0 +1,14 @@
+function is_Ip_Adress_OrNot(text) {
+    var array_text = text.split('.');
+    if (!(array_text.length == 4)) {
+        return false;
+    }
+    for (let x of array_text) {
+        if (!((0 <= x) && (x <= 255))) {
+            return false;
+        }
+    }
+    return true;
+
+};
+console.log(is_Ip_Adress_OrNot('250.255.-12.14'));
